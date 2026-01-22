f1a788d321b3e3d4481dff4f65bb24ebbd87107a
Arevalo Asuaje Perez <arevalo@macbookpro.lan>
2026-01-21 21:00:40 -0300

    actualizando android

--- tests/screens/landing.screen.js
+++ tests/screens/landing.screen.js
@@ -1,8 +1,55 @@
-const { iosPredicateName, accessibilityId } = require('../utils/selectors');
+const {
+  iosPredicateName,
+  accessibilityId,
+  androidResourceId,
+} = require('../utils/selectors');
 
 class LandingScreen {
   get loginButton() {
     return $(driver.isAndroid ? accessibilityId('Login') : iosPredicateName('Login'));
+  }
+
+  get androidAnrTitle() {
+    return $(androidResourceId('android:id/alertTitle'));
+  }
+
+  get androidAnrCloseButton() {
+    return $(androidResourceId('android:id/aerr_close'));
+  }
+
+  get androidAnrWaitButton() {
+    return $(androidResourceId('android:id/aerr_wait'));
+  }
+
+  async dismissAndroidAnrIfPresent() {
+    if (!driver.isAndroid) {
+      return false;
+    }
+
+    if (!(await this.androidAnrTitle.isExisting())) {
+      return false;
+    }
+
+    const title = await this.androidAnrTitle.getText();
+    if (!/isn\'t responding|is not responding|no responde/i.test(title || '')) {
+      return false;
+    }
+
+    if (await this.androidAnrCloseButton.isExisting()) {
+      await this.androidAnrCloseButton.click();
+    } else if (await this.androidAnrWaitButton.isExisting()) {
+      await this.androidAnrWaitButton.click();
+    } else {
+      return false;
+    }
+
+    await browser.waitUntil(async () => !(await this.androidAnrTitle.isExisting()), {
+      timeout: 10000,
+      interval: 500,
+      timeoutMsg: 'Android ANR dialog is still visible.',
+    });
+
+    return true;
   }
 
   async relaunchApp() {
@@ -19,13 +66,16 @@ class LandingScreen {
   }
 
   async ensureOnLanding() {
+    await this.dismissAndroidAnrIfPresent();
     try {
       await this.loginButton.waitForDisplayed({ timeout: 15000 });
       return;
     } catch (error) {
+      await this.dismissAndroidAnrIfPresent();
       await this.relaunchApp();
     }
 
+    await this.dismissAndroidAnrIfPresent();
     await this.loginButton.waitForDisplayed({ timeout: 20000 });
   }
 
